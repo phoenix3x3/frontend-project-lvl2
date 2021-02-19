94cf8d44b666f649f8c0e0091620131ed9e51ffe
Phoenix3x3 <andrey.bernatski2000@gmail.com>
2021-02-19 19:44:58 +0300

    add -f option and filepath args

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 const program = require('commander');
 
-program.version('0.0.1');
-
-program.description('Compares two configuration files and shows a difference.');
+program
+  .version('0.0.1')
+  .arguments('<filepath1>')
+  .arguments('<filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
