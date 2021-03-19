8cd762ec79d814c0ebd2a49cb5bc69b154e98e9f
Phoenix3x3 <andrey.bernatski2000@gmail.com>
2021-03-19 12:35:47 +0300

    refact gendiff.test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,31 +6,23 @@ import genDiff from '../src/genDiff.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const filepath1 = path.resolve(__dirname, '__fixtures__/file1.json');
-const filepath2 = path.resolve(__dirname, '__fixtures__/file2.json');
-const filepath3 = path.resolve(__dirname, '__fixtures__/file1.yml');
-const filepath4 = path.resolve(__dirname, '__fixtures__/file2.yml');
+const jsonFilePath1 = path.resolve(__dirname, '__fixtures__/file1.json');
+const jsonFilePath2 = path.resolve(__dirname, '__fixtures__/file2.json');
+const ymlFilePath1 = path.resolve(__dirname, '__fixtures__/file1.yml');
+const ymlFilePath2 = path.resolve(__dirname, '__fixtures__/file2.yml');
 
-// const PlainPath
-const resultPath = path.resolve(__dirname, '__fixtures__/expectedStylishFormat.txt');
-const resultPathPlain = path.resolve(__dirname, '__fixtures__/exptectedPlainFormat.txt');
-const resultPathJson = path.resolve(__dirname, '__fixtures__/expectedJsonFormat.txt');
-const result = fs.readFileSync(resultPath, 'utf8');
-const resultPlain = fs.readFileSync(resultPathPlain, 'utf8');
-const resultJson = fs.readFileSync(resultPathJson, 'utf8');
+const getResult = (format) => {
+  const resultPath = path.resolve(__dirname, `__fixtures__/expected-${format}.txt`);
+  return fs.readFileSync(resultPath, 'utf8');
+};
 
-test('genDiff json', () => {
-  expect(genDiff(filepath1, filepath2)).toBe(result);
-});
-test('genDiff yml', () => {
-  expect(genDiff(filepath3, filepath4)).toBe(result);
-});
-test('genDiff json plainFormat', () => {
-  expect(genDiff(filepath1, filepath4, 'plain')).toBe(resultPlain);
-});
-test('genDiff json jsonFormat', () => {
-  expect(genDiff(filepath3, filepath2, 'json')).toBe(resultJson);
-});
-test('genDiff yml jsonFormat', () => {
-  expect(genDiff(filepath3, filepath4, 'json')).toBe(resultJson);
+describe.each`
+  filePath1        | filePath2        | format       | expected
+  ${jsonFilePath1} | ${jsonFilePath2} | ${'stylish'} | ${getResult('stylish')}
+  ${ymlFilePath1}  | ${ymlFilePath2}  | ${'plain'}   | ${getResult('plain')}
+  ${jsonFilePath1} | ${ymlFilePath2}  | ${'json'}    | ${getResult('json')}
+`('$format format', ({ filePath1, filePath2, format, expected }) => {
+  test(`gendiff`, () => {
+    expect(genDiff(filePath1, filePath2, format)).toBe(expected);
+  });
 });
